a2a346ef780ceae2b399345eb1f05d1be98d7c7b
th-release <th.release.cth@gmail.com>
2022-08-20 12:50:12 +0900

    GBSW-API : ESS

--- src/api/ess/ess.controller.ts
+++ src/api/ess/ess.controller.ts
@@ -1,5 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { Req, Res, Post } from '@nestjs/common';
 import { Request, Response } from 'express';
+
+import { EssService } from './ess.service';
 @Controller('api/ess')
-export class EssController {}
+export class EssController {
+  constructor (
+    private readonly authService: EssService,
+  ) {}
+}
